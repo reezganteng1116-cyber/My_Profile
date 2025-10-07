47d368e2773630ddbe52a379856f7fb1abf9a9e6
jaisyurrahman-arch <jaisyurrahman3@gmail.com>
2025-10-07 17:49:26 +0700

    delete something

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,9 +27,7 @@ function Skills() {
     { name: "Tailwind", img: "images/Icon_Tailwind.png" },
     { name: "Node.js", img: "/images/Icon_NodeJS.png" },
     { name: "Vite", img: "/images/Icon_Vite.png" },
-    { name: "React-Native", img: "/images/Icon_ReactNative.png" },
-    { name: "Dart", img: "/images/Icon_Dart.png" },
-    { name: "Bootstrap", img: "/images/Icon_Bootstrap.png" },
+    { name: "React-Native", img: "/images/Icon_ReactNative.png" }
   ];
 
   return (
